f97dd7737584a2821e55ad93cdc7209f4fc6d2b5
yu-takahashi <yuta.takahashi1996@gmail.com>
2024-04-14 17:19:05 +0900

    fix: modelの修正

--- models/category.py
+++ models/category.py
@@ -1,17 +1,15 @@
 import datetime
-from sqlalchemy import Column, DateTime, Integer, Null, String, Boolean, func, Text
+from sqlalchemy import Column, DateTime, Integer, Null, String, Boolean, func
+from datetime import timedelta
 from database import Base
 
 
-class Article(Base):
-    __tablename__ = "articles"
+class Category(Base):
+    __tablename__ = "categories"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    category_id = Column(Integer, nullable=False)
-    title = Column(String(255), nullable=True)
-    content = Column(Text(), nullable=False)
+    category_name = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
-    create_user_id = Column(Integer, nullable=True)
     created_at = Column(DateTime, default = func.now(), nullable=True)
     updated_at = Column(DateTime, default = func.now(), onupdate=func.now(), nullable=True)
     deleted_at = Column(DateTime, nullable=True)
