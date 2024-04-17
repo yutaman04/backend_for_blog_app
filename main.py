76cc0f9a4e0dacaf8f7dd87a7f165a99d62eb7e5
yu-takahashi <yuta.takahashi1996@gmail.com>
2024-04-17 23:34:34 +0900

    feat: 記事一覧と記事詳細でUserテーブルを結合してユーザー名を取得できるよう修正

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ sys.path.append(project_root)
 print(project_root)
 
 
+from sqlalchemy import Null
 import strawberry
 from strawberry.asgi import GraphQL
 from fastapi import FastAPI,Request
@@ -20,46 +21,79 @@ zoneinfo.ZoneInfo('Asia/Tokyo')
 from database import SessionLocal
 from models.category import Category as CategoryModel
 from models.article import Article as ArticleModel
+from models.user import User as UserModel
 
 @strawberry.type
 class Category:
-    id: int
+    id: strawberry.ID
     categoryName: str
     
 @strawberry.type
-class Ariticle:
-    id: int
+class Article:
+    id: strawberry.ID
     categoryId: int
     title: str
     content: str
     isActive: bool
     createUserId: int
+    createUserName: str
+    createUserDisplayName: str
     createdAt: str
     updatedAt: str
 
 @strawberry.type
 class Query:
+    # カテゴリー一覧取得
     @strawberry.field
     def categories(self) -> list[Category]:
         db: Session = SessionLocal() 
         data = db.query(CategoryModel).all()
             
         return [Category(id=cat.id, categoryName=cat.category_name) for cat in data]
+
+    # 記事一覧取得（ページネート無）
     @strawberry.field
-    def articles(self) -> list[Ariticle]:
+    def articles(self) -> list[Article]:
         db: Session = SessionLocal()
-        data = db.query(ArticleModel).all()
-        print(data[0].title)
-        return [Ariticle(
-                        id=article.id,
-                        categoryId=article.category_id,
-                        title=article.title,
-                        content=article.content,
-                        isActive=article.is_active,
-                        createUserId=article.create_user_id,
-                        createdAt=article.created_at,
-                        updatedAt=article.updated_at
-                    ) for article in data]
+        data = db.query(ArticleModel, UserModel).join(UserModel, UserModel.id == ArticleModel.create_user_id).all()
+
+        if data:
+            return [Article(
+                    id=article.Article.id,
+                    categoryId=article.Article.category_id,
+                    title=article.Article.title,
+                    content=article.Article.content,
+                    isActive=article.Article.is_active,
+                    createUserId=article.Article.create_user_id,
+                    createUserName=article.User.user_name,
+                    createUserDisplayName=article.User.display_name,
+                    createdAt=article.Article.created_at,
+                    updatedAt=article.Article.updated_at
+                ) for article in data]
+        else:
+            raise Exception("Articles not found")
+
+    # 記事取得
+    @strawberry.field
+    def article(self, info, id: strawberry.ID) -> Article:
+        db: Session = SessionLocal()
+        article = db.query(ArticleModel, UserModel).join(UserModel, UserModel.id == ArticleModel.create_user_id).filter(ArticleModel.id == id).first()
+        
+        if article:
+            return Article(
+                    id=article.Article.id,
+                    categoryId=article.Article.category_id,
+                    title=article.Article.title,
+                    content=article.Article.content,
+                    isActive=article.Article.is_active,
+                    createUserId=article.Article.create_user_id,
+                    createUserName=article.User.user_name,
+                    createUserDisplayName=article.User.display_name,
+                    createdAt=article.Article.created_at,
+                    updatedAt=article.Article.updated_at
+                )
+        else:
+            raise Exception("Article not found")
     
     
 schema = strawberry.Schema(query=Query)
