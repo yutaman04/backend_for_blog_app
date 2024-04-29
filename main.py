fdf59a63e54d258911488e17aba71af9faf02ddc
yu-takahashi <yuta.takahashi1996@gmail.com>
2024-04-29 16:44:31 +0900

    パーミッション

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+import os
+import sys
+
+from api.schema.graphql_schema import Query
+# 現在のスクリプトのディレクトリを取得
+current_dir = os.path.dirname(__file__)
+
+# プロジェクトルートのパスを取得
+project_root = os.path.abspath(os.path.join(current_dir, '.'))
+
+# プロジェクトルートをPythonパスに追加
+sys.path.append(project_root)
+print(project_root)
+
+
+from sqlalchemy import Null
+import strawberry
+from strawberry.asgi import GraphQL
 from fastapi import FastAPI,Request
+from sqlalchemy.orm import Session
+import zoneinfo
+zoneinfo.ZoneInfo('Asia/Tokyo')
+    
+schema = strawberry.Schema(query=Query)
+graphql_app = GraphQL(schema)
 
 app = FastAPI()
-
+app.add_route("/api/graphql", graphql_app)
 
 @app.get("/api")
 async def root(request:Request):
