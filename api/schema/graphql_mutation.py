6bae423e6638bf147aaa9b47a7d3d111c784e8a4
yu-takahashi <yuta.takahashi1996@gmail.com>
2024-06-13 00:17:38 +0900

    feat: 画像アップロードAPIの追加

--- api/schema/graphql_mutation.py
+++ api/schema/graphql_mutation.py
@@ -1,8 +1,11 @@
+from fastapi import File
+from api.service.article_service import ArticleService
 from api.service.auth_service import AuthService
-from api.schema.graphql_schema import AuthResult, AuthVerificationResult
+from api.schema.graphql_schema import AdminArticleUpload, AuthResult, AuthVerificationResult
 import strawberry
 import zoneinfo
 zoneinfo.ZoneInfo('Asia/Tokyo')
+from strawberry.file_uploads import Upload
 
 
 @strawberry.type
@@ -15,4 +18,22 @@ class Mutation:
     @strawberry.mutation
     def jwt_verification(self, target_jwt: str) -> AuthVerificationResult:
         auth_service = AuthService()
+        
         return auth_service.jwt_verification(target_jwt)
+    
+    @strawberry.mutation
+    async def article_image_upload(self, jwt: str,  file: Upload) -> AdminArticleUpload:
+        
+        # 認証
+        if jwt is None:
+            raise ValueError("jwt is required for upload article image")
+        
+        auth_service = AuthService()
+        auth_reuslt = auth_service.jwt_verification(jwt)
+        if auth_reuslt.msg != 'success':
+            return AdminArticleUpload(status="auth_error", filePath="")
+        
+        article_service = ArticleService()
+        export_file_path = await article_service.article_image_upload(file)
+        
+        return AdminArticleUpload(status="success", filePath=export_file_path)
